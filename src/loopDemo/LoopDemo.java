463c9bc12c5bc455224c681b78e3d96b8a320941
Isaac-McClure <isaac.mcclure.3@gmail.com>
2021-10-30 21:08:18 +1100

    Finished adding examples for different loop types and keywords.

--- src/loopDemo/LoopDemo.java
+++ src/loopDemo/LoopDemo.java
@@ -5,8 +5,87 @@ package loopDemo;
 
 /**
  * @author Isaac McClure
- * Date: 30-Oct-21
+ * Date: 30-October-21
  */
-public class LoopDemo {
-
+public class LoopDemo 
+{
+	public void whileLoop() 
+	{
+		int loopCounter = 1;
+		
+		while(loopCounter < 5)
+		{
+			System.out.print("I am printing in a loop: " + loopCounter + "\n");	
+			loopCounter += 1;
+		}
+		
+		System.out.println();	
+	}
+	
+	public void doWhileLoop() 
+	{
+		int loopCounter = 1;
+		do
+		{
+			System.out.print("I am printing in a loop: " + loopCounter + "\n");	
+			loopCounter += 1;
+		}
+		while(loopCounter == -400);
+	}
+	
+	public void forLoop()
+	{
+		String[] words = {"Word 1", "Word 2", "Word 3"};
+		
+		int numberOfWords = words.length;
+		
+		for(int wordNumber = 0; wordNumber < numberOfWords; wordNumber ++)
+		{	
+			System.out.print("I am printing words in a loop: " + words[wordNumber] + "\n");			
+		}
+	}
+	
+	public void forEachLoop()
+	{
+		String[] words = {"Word 1", "Word 2", "Word 3"};
+				
+		for(String word: words)
+		{
+			System.out.print("I am printing words in a loop: " + word + "\n");			
+		}
+	}
+	
+	public void breakExample()
+	{
+		String[] words = {"Word 1", "Word 2", "Word 3"};
+		
+		int numberOfWords = words.length;
+		
+		for(int wordNumber = 0; wordNumber < numberOfWords; wordNumber ++)
+		{	
+			if(wordNumber > 0)
+			{
+				break;				
+			}
+				
+			System.out.print("I am printing words in a loop: " + words[wordNumber] + "\n");			
+		}
+	}
+	
+	public void continueExample()
+	{
+		String[] words = {"Word 1", "Word 2", "Word 3"};
+		
+		int numberOfWords = words.length;
+		
+		for(int wordNumber = 0; wordNumber < numberOfWords; wordNumber ++)
+		{	
+			if(wordNumber == 1)
+			{
+				continue;				
+			}
+				
+			System.out.print("I am printing words in a loop: " + words[wordNumber] + "\n");			
+		}
+	}
 }
---
+++ src/loopDemo/Main.java
@@ -0,0 +1,20 @@
+package loopDemo;
+
+public class Main 
+{	
+	public static void main(String[] args) 
+	{
+		LoopDemo myLoopDemo = new LoopDemo();		
+		myLoopDemo.whileLoop();	
+		System.out.println();	
+		myLoopDemo.doWhileLoop();
+		System.out.println();	
+		myLoopDemo.forLoop();
+		System.out.println();	
+		myLoopDemo.forEachLoop();
+		System.out.println();	
+		myLoopDemo.breakExample();
+		System.out.println();	
+		myLoopDemo.continueExample();
+	}
+}
